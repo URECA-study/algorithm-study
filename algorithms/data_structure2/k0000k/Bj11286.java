8ac237b008ab3f56d4e9694d40f6aebdb75c05c9
k0000k <locker50@naver.com>
2025-02-18 10:27:38 +0900

    [ADD] Solve BJ 11286

---
+++ algorithms/data_structure2/k0000k/Bj11286.java
@@ -0,0 +1,37 @@
+package data_structure2.k0000k;
+
+import java.io.*;
+import java.util.*;
+
+public class Bj11286 {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (Math.abs(o1) == Math.abs(o2)) {
+                return o1 - o2;
+            }
+            return Math.abs(o1) - Math.abs(o2);
+        }
+    });
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int oper = Integer.parseInt(br.readLine());
+            if (oper == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                }
+                else {
+                    System.out.println(pq.poll());
+                }
+            }
+            else {
+                pq.add(oper);
+            }
+        }
+    }
+
+}
